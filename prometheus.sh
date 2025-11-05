deacae7e539aedeae89272eed23f1c866f185396
Haeckal Al Kaaf <haeckal.alkaaf@finansys.com>
2025-11-05 20:22:33 +0700

    update

--- prometheus.sh
+++ prometheus.sh
@@ -84,3 +84,9 @@ sudo mkdir -p /var/lib/prometheus/
 sudo systemctl daemon-reload
 sudo systemctl start prometheus
 sudo systemctl enable prometheus
+
+Installing Node Exporter
+
+1. Download Node Exporter in this website https://github.com/prometheus/node_exporter/releases/tag/v1.10.2
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.10.2/node_exporter-1.10.2.linux-amd64.tar.gz
